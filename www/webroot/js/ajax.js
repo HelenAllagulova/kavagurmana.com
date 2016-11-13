1fd1ff0851c074f7298ec74d2cdb76eead2259fb
HelenAllagulova <helen.allagulova@gmail.com>
2016-11-13 23:19:31 +0200

    cookie class

--- www/webroot/js/ajax.js
+++ www/webroot/js/ajax.js
@@ -1,4 +1,4 @@
-function ajax(path, loyout_flag = '0', result){
+function ajax(path, loyout_flag ='0', result){
     loyout_flag='loyout_flag='+loyout_flag;
     $.post(path, {loyout_flag:loyout_flag}, function(data){
         $("#"+result).text(data);
