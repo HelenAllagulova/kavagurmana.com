6c2a84daad389258fb997c9cd261cca0962c1ffc
HelenAllagulova <helen.allagulova@gmail.com>
2016-11-15 23:31:22 +0200

    cart indicator

--- www/controllers/cart.controller.php
+++ www/controllers/cart.controller.php
@@ -19,11 +19,8 @@ class CartController extends Controller{
     public function __construct($data = array())
     {
         parent::__construct($data);
-
         if(!Cookie::get('cart') == null){
-
             $this->products = json_decode(Cookie::get('cart'), true);
-            var_dump($this->products);
         }
     }
 
@@ -36,7 +33,8 @@ class CartController extends Controller{
     public function getProducts($for_sql = false)
     {
         if ($for_sql) {
-            return implode(',', $this->products);
+
+            return implode(',', array_keys($this->products));
         }
 
         return $this->products;
@@ -50,21 +48,29 @@ class CartController extends Controller{
      */
     public function addProduct($quantity=1)
     {
-
         $id = (int)($this->params[0]);
-
         if (!array_key_exists($id, $this->products)) {
-            $this->products[$id] = $quantity;
+            $this->products[$id] =(int)($quantity);
         }
-
         Cookie::set('cart', json_encode($this->products));
-
-        $count_products = count($this->products);
-        var_dump($count_products);
-//        $ids_sql = $this->getProducts(true);
-//        $this->model = new ProductsModel();
-//        $this->data['cart'] = $this->model->getByIds($ids_sql);
-//        var_dump($this->data['cart']);
+        $count_products = array_sum($this->products);
+        $ids_sql = $this->getProducts(true);
+        $this->model = new ProductsModel();
+        $this->data['cart'] = $this->model->getByIds($ids_sql);
+        $sum=[];
+        $all_sum = 0;
+             foreach ($this->products as $key => $value){
+                for ($i=0; $i< count($this->data['cart']); $i++){
+                    if((int)($this->data['cart'][$i]['id'])==$key){
+                        $sum[$key]= $this->data['cart'][$i]['price']*$value;
+                        $all_sum += $sum[$key];
+                    }
+                  continue;
+                }
+            }
+        $cart_str = "<b>{$count_products}</b> товаров на сумму <b>{$all_sum}</b> грн.";
+        echo $cart_str;
+        die();
     }
 
 
